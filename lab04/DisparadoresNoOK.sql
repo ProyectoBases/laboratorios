985dbb8d37d147ad276df94f6fb17d20b020e6af
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-03-20 18:55:43 -0500

    Se crearon disparadores NO OK

---
+++ lab04/DisparadoresNoOK.sql
@@ -0,0 +1,13 @@
+----------DisparadoresNoOK----------
+
+--planFormacion--
+
+--ya probados--
+INSERT INTO planFormacion VALUES(0,TO_DATE('10/April/2018'),'activo','sergioarb_@gmail.com',TO_DATE('22/April/2018'),'gualdronsito@hotmail.com');
+INSERT INTO planFormacion VALUES(0,TO_DATE('10/April/2018'),'activo','pepita@hotmail.com',TO_DATE('22/April/2018'),'vale_gon@hotmail.com');
+
+--no probados--
+INSERT INTO planFormacion VALUES(0,TO_DATE('10/May/2018'),'activo','sarapupo@yahoo.es',TO_DATE('27/April/2018'),'davidsaa@hotmail.com');
+INSERT INTO planFormacion VALUES(0,TO_DATE('2/April/2018'),'activo','juliana@gmail.com',TO_DATE('22/April/2018'),'luisabmdez@gmail.com');
+INSERT INTO planFormacion VALUES(0,TO_DATE('10/May/2018'),'activo','david_@hotmail.com',TO_DATE('22/April/2018'),'joseduran@gmail.com');
+INSERT INTO planFormacion VALUES(0,TO_DATE('3/May/2018'),'activo','pabloneruda@yahoo.es',TO_DATE('22/April/2018'),'santiago_rocha_@hotmail.com');
