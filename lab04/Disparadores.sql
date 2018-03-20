bf900b51a4128e398913f8cac059d67731880f0f
unknown <2128884@labinfo.is.escuelaing.edu.co>
2018-03-20 18:01:53 -0500

    se agregaron triggers

--- lab04/Disparadores.sql
+++ lab04/Disparadores.sql
@@ -1,11 +1,18 @@
+---MANTENER PLAN FORMACIÓN-----
 --Se asigna automaticamente el número de PlanFormación--
 
 CREATE OR REPLACE TRIGGER AD_planFormacion_numero
 BEFORE INSERT ON planFormacion
 FOR EACH ROW
-DECLARE cont NUMBER;
+DECLARE
+cont NUMBER;
+p NUMBER;
 BEGIN
-SELECT COUNT(numero)+1 INTO cont FROM planFormacion;
+SELECT MAX(numero)+1 INTO cont FROM planFormacion;
+SELECT COUNT(numero) INTO cont FROM planFormacion;
+IF (p = 0) THEN
+cont:=1;
+END IF;
 :NEW.numero := cont;
 END;
 /
@@ -38,11 +45,32 @@ END;
 CREATE OR REPLACE TRIGGER AD_planFormacion_planFormacion
 BEFORE INSERT ON planFormacion
 FOR EACH ROW
-DECLARE piv NUMBER;
+DECLARE 
+piv NUMBER;
 BEGIN
-SELECT COUNT(candidato) INTO piv FROM planFormacion WHERE (correoCandidato = :NEW.correoCandidato) AND (EXTRACT(YEAR FROM :NEW.fecha) = EXTRACT(YEAR FROM SYSDATE));
+SELECT COUNT(correoCandidato) INTO piv FROM planFormacion WHERE (correoCandidato = :NEW.correoCandidato) AND (EXTRACT(YEAR FROM :NEW.fecha) = EXTRACT(YEAR FROM SYSDATE));
 IF (piv >0) THEN
-RISE_APPLICATION_ERROR(-20001,'paila');
+RAISE_APPLICATION_ERROR(-20000, 'no es posible insertar dos veces en el mismo año.');
+END IF;
+IF (EXTRACT(MONTH FROM SYSDATE)<>1) THEN
+RAISE_APPLICATION_ERROR(-20000, 'no se puede insertar en este año');
 END IF;
 END;
 /
+
+---El evaluador no puede ser uno de los compañeros (candidatos)---
+
+
+CREATE OR REPLACE TRIGGER AD_planFormacion_evaluador
+BEFORE INSERT ON planFormacion
+FOR EACH ROW
+DECLARE
+co number;
+BEGIN
+SELECT COUNT(correo) INTO co FROM candidato WHERE :NEW.evaluador = correo;
+IF (co>0) THEN
+RAISE_APPLICATION_ERROR(-20000, 'un evaluador no puede ser un candidato');
+END IF;
+END;
+/
+
