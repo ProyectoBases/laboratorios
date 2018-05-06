dd309bee75d30a4ad374faaca81afa7ec28e4abb
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-05-06 16:54:28 -0500

    correccion CRUDE

--- lab06/CRUDE.sql
+++ lab06/CRUDE.sql
@@ -3,9 +3,10 @@
 ---1. Mantener Plan Formacion---
 CREATE OR REPLACE PACKAGE PC_PLANDEFORMACION IS
 
-PROCEDURE Adicionar_Plan (numero NUMBER,fecha DATE,estado VARCHAR,evaluador VARCHAR, correoCandidato VARCHAR);
-PROCEDURE Adicionar_Prioridad (prioridad VARCHAR);
-PROCEDURE Modificar_Plan (numero NUMBER, estado VARCHAR, evaluador VARCHAR, fechaFin DATE);
+PROCEDURE Adicionar_Plan (id NUMBER, inicio DATE, habilitado VARCHAR, profe VARCHAR, correo VARCHAR);
+PROCEDURE Adicionar_Prioridad (Tprioridad VARCHAR);
+PROCEDURE Modificar_Plan (id NUMBER, habilitado VARCHAR, profe VARCHAR, final DATE);
+PROCEDURE Modificar_Prioridad (numeroP NUMBER, nombreH VARCHAR, Tprioridad VARCHAR);
 FUNCTION Consultar_Forma_Hab RETURN SYS_REFCURSOR;
 FUNCTION Consultar_Cal_Candidato RETURN SYS_REFCURSOR;
 
@@ -15,9 +16,9 @@ END PC_PLANDEFORMACION;
 
 CREATE OR REPLACE PACKAGE PC_CURSOS IS
 
-PROCEDURE Adicionar_Curso (codigo VARCHAR,nombre VARCHAR, cerrado NUMBER);
-PROCEDURE Modificar_Curso (codigo VARCHAR, detalle VARCHAR, cerrado NUMBER);
-PROCEDURE Eliminar_Curso (codigo VARCHAR);
+PROCEDURE Adicionar_Curso (id VARCHAR, name VARCHAR, habilitado NUMBER);
+PROCEDURE Modificar_Curso (id VARCHAR, info VARCHAR, habilitado NUMBER);
+PROCEDURE Eliminar_Curso (id VARCHAR);
 FUNCTION Consultar_Curso RETURN XMLTYPE;
 FUNCTION Consultar_CursoHab(habilidad VARCHAR) RETURN SYS_REFCURSOR;
 
