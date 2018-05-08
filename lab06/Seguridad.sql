769c4436ead16b883e9288ec62aa1f18cd9aa565
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-05-08 15:31:03 -0500

    correccion

--- lab06/Seguridad.sql
+++ lab06/Seguridad.sql
@@ -3,12 +3,11 @@
 GRANT EXECUTE ON PA_JUNTA TO bd2127790;
 
 --CREAMOS EL ROL CANDIDATOS--
-CREATE ROLE Candidatoos;
+CREATE ROLE Candidatos_;
 --DAMOS PERMISOS AL ROL CANDIDATOS--
-GRANT EXECUTE ON PA_CANDIDATO TO Candidatoos;
+GRANT EXECUTE ON PA_CANDIDATO TO Candidatos_;
 --Asumiendo que dos de sus compañeros de curso (no del equipo) son candidatos, asígnenles ese rol.--
 
 
-GRANT Candidatoos TO bd2125666;
-GRANT Candidatoos TO bd2135429;
-
+GRANT Candidatos_ TO bd2125666;
+GRANT Candidatos_ TO bd2135429;
