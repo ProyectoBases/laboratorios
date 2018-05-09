d9e88ec4d065419e2260bdad023a0f24fb00562c
Nattpalacios <36208100+Nattpalacios@users.noreply.github.com>
2018-05-09 13:33:46 -0500

    correccion

--- lab06/ActoresE.sql
+++ lab06/ActoresE.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE PACKAGE PA_JUNTA IS
 
 PROCEDURE Adicionar_Plan (id NUMBER, inicio DATE, habilitado VARCHAR, profe VARCHAR, correo VARCHAR);
-PROCEDURE Adicionar_Prioridad (Tprioridad VARCHAR,nombreH VARCHAR,numeroP NUMBER);
+PROCEDURE Adicionar_Prioridad (Tprioridad VARCHAR, nombreH VARCHAR, numeroP NUMBER);
 PROCEDURE Modificar_Plan (id NUMBER, habilitado VARCHAR, profe VARCHAR, final DATE);
 PROCEDURE Modificar_Prioridad (numeroP NUMBER, nombreH VARCHAR, Tprioridad VARCHAR);
-PROCEDURE Adicionar_Curso (id VARCHAR, name VARCHAR, habilitado NUMBER);
+PROCEDURE Adicionar_Curso (id VARCHAR, name VARCHAR, habilitado NUMBER, nombreH VARCHAR);
 PROCEDURE Modificar_Curso (id VARCHAR, info VARCHAR, habilitado NUMBER);
 PROCEDURE Eliminar_Curso (id VARCHAR);
 FUNCTION Consultar_Info_Candidato RETURN SYS_REFCURSOR;
