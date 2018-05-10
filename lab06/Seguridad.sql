54c5f3b355b2496a9bc5a9998b6fc457edef6b44
AndresFelipeGualdron <gualdronsito@hotmail.com>
2018-05-10 18:20:42 -0500

    terminado

--- lab06/Seguridad.sql
+++ lab06/Seguridad.sql
@@ -20,6 +20,9 @@ GRANT EXECUTE ON PA_JUNTA TO bd2127790;
 --CREAMOS EL ROL CANDIDATOS--
 CREATE ROLE Candidatoss;
 --DAMOS PERMISOS AL ROL CANDIDATOS--
+GRANT SELECT ON candidato TO candidatoss;
+GRANT UPDATE ON candidato TO candidatoss;
+GRANT INSERT ON candidato TO candidatoss;
 GRANT EXECUTE ON PA_CANDIDATO TO Candidatoss;
 --Asumiendo que dos de sus compañeros de curso (no del equipo) son candidatos, asígnenles ese rol.--
 
